95711b31577ffce335d4cf74eb7b6bd0ccc0ac76
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-01 12:24:59 +0200

    add: chatId page/mobile toggle/sheet component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,51 @@
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { currentProfile } from "@/lib/current-profile";
+import ChatHeader from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({
+  params: { serverId, channelId },
+}: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId,
+      profileId: profile?.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        type="channel"
+        name={channel.name}
+        serverId={channel.serverId}
+      />
+    </div>
+  );
 };
 export default ChannelIdPage;
