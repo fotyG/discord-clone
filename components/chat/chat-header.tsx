639bbdcb73a68668d0acc134e092467162287006
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-01 14:02:29 +0200

    add: full socket.io setup/utils/boilerplate/types add: badge indicator component for successful socket.io connection

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 
 import UserAvatar from "@/components/user-avatar";
 import MobileToggle from "@/components/mobile-toggle";
+import SocketIndicator from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   name: string;
@@ -24,6 +25,9 @@ const ChatHeader = ({ name, type, serverId, imageUrl }: ChatHeaderProps) => {
         />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
